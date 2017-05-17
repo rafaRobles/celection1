cc5e09b4d5937223bb5e28198b7c4ffc4a00b9d1
rafaRobles <selbor1@hotmail.com>
2017-05-17 17:19:02 -0500

    Add files via upload

--- android_asset/funcionesVerEquiposFiltro.js
+++ android_asset/funcionesVerEquiposFiltro.js
@@ -1,4 +1,4 @@
-﻿selectEquipos()
+selectEquipos()
 
 function selectEquipos()
 {
@@ -17,13 +17,13 @@ function selectEquipos()
 	valores = 'getEquiposFiltro.php?presupuesto='+presupuesto+'&calidadCamara='+calidadCamara+"&memoria="+memoria+"&desempeño="+desempeno
 	console.log(valores)
 		ajax = new XMLHttpRequest();
-		ajax.open('GET','http://celection.esy.es/'+valores,true);
+		ajax.open('GET',valores,true);
 		ajax.setRequestHeader("Content-type", "application/json; charset=utf-8");
 		ajax.setRequestHeader("Data-type", "jsonp");
 		//ajax.setRequestHeader("Access-Control-Allow-Origin");
 		ajax.send();
-		ajax.onreadystatechange = function(){
-			if(ajax.readyState == 4 && ajax.status==200){
+		ajax.onreadystatechange = function()
+{			if(ajax.readyState == 4 && ajax.status==200){
 					data =JSON.parse(ajax.responseText)
 					console.log(data)
 					var mostrarDatos ="";
@@ -35,17 +35,17 @@ function selectEquipos()
 											'<img src="'+data.valores[i].imgRuta+'">'+	
 										'</div>'+
 										'<div class="infoEquipo">'+
-											'<p>Modelo:</p> <p>'+ data.valores[i].modelo_equipo +'</p>'+
-											'<p>Marca:</p> <p>'+ data.valores[i].marca_equipo +'</p>'+
-											'<p>Precio:</p> <p>'+ data.valores[i].precio +'</p>'+
-											'<p>Camara:</p> <p>'+ data.valores[i].camara_px +'</p>'+
-											'<p>Camara Frontal:</p> <p>'+ data.valores[i].camara_frontal_px +'</p>'+
-											'<p>Memoria Interna:</p> <p>'+ data.valores[i].memoria_interna +'</p>'+
-											'<p>Memoria Externa:</p> <p>'+ data.valores[i].memoria_externa +'</p>'+
-											'<p>Ram:</p> <p>'+ data.valores[i].ram +'</p>'+
-											'<p>Tamaño:</p> <p>'+ data.valores[i].tamaño +'</p>'+
-											'<p>Resolucion de Pantalla:</p> <p>'+ data.valores[i].resolucion_pantalla +'</p>'+
-											'<p>Gama:</p> <p>'+ data.valores[i].gama +'</p>'+
+											'<p class="pEstatico">Modelo:</p> <p class="pDinamico">'+ data.valores[i].modelo_equipo +'</p>'+
+											'<p class="pEstatico">Marca:</p> <p class="pDinamico">'+ data.valores[i].marca_equipo +'</p>'+
+											'<p class="pEstatico">Precio:</p> <p class="pDinamico">'+ data.valores[i].precio +'</p>'+
+											'<p class="pEstatico">Camara:</p> <p class="pDinamico">'+ data.valores[i].camara_px +'</p>'+
+											'<p class="pEstatico">Camara Frontal:</p> <p class="pDinamico">'+ data.valores[i].camara_frontal_px +'</p>'+
+											'<p class="pEstatico">Memoria Interna:</p> <p class="pDinamico">'+ data.valores[i].memoria_interna +'</p>'+
+											'<p class="pEstatico">Memoria Externa:</p> <p class="pDinamico">'+ data.valores[i].memoria_externa +'</p>'+
+											'<p class="pEstatico">Ram:</p> <p class="pDinamico">'+ data.valores[i].ram +'</p>'+
+											'<p class="pEstatico">Tamaño:</p> <p class="pDinamico">'+ data.valores[i].tamaño +'</p>'+
+											'<p class="pEstatico">Resolucion de Pantalla:</p> <p class="pDinamico">'+ data.valores[i].resolucion_pantalla +'</p>'+
+											'<p class="pEstatico">Gama:</p> <p class="pDinamico">'+ data.valores[i].gama +'</p>'+
 										'</div>'+	
 									'</div>'		
 					}
